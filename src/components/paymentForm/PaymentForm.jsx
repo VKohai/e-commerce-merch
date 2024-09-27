f48f83a2f9c464716b89020af609928bb1d0a2c1
VKohai <kohai.entertainment@gmail.com>
2024-09-27 14:49:26 +0300

    Add Payment Form Adaptation

--- src/components/paymentForm/PaymentForm.jsx
+++ src/components/paymentForm/PaymentForm.jsx
@@ -2,10 +2,19 @@ import "./paymentForm.scss";
 import * as Yup from 'yup';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
+function validateFullName(value) {
+    const words = value.trim().split(" ");
+    if (words.length < 2) {
+        return "ФИО должно состоять минимум из фамилии и имени.";
+    }
+}
+
 const paymentScheme = Yup.object().shape({
     fullname: Yup.string().min(2, "Минимум 2 символа").required("Введите ФИО получателя."),
     email: Yup.string().email("Неверный формат почты").required("Введите почту, отправим вам чек."),
-    phoneNumber: Yup.string().required("Введите ваш номер телефона, куда придет уведомление о получении.")
+    phoneNumber: Yup.string().required("Введите ваш номер телефона, куда придет уведомление о получении."),
+    address: Yup.string().required("Введите адрес доставки."),
+    zipcode: Yup.string().min(6, "Минимальное количество символов 6").required("Введите индекс почты")
 });
 
 const PaymentForm = () => {
@@ -33,28 +42,26 @@ const PaymentForm = () => {
                                     e.preventDefault();
                                     console.log(values);
                                 }}>
-                                    <Field type="text" name="fullname" placeholder="ФИО" id="fullname" values={values.fullname} />
-                                    <ErrorMessage name="fullname" component="div" />
+                                    <ErrorMessage name="fullname" component="div" className="form-error" />
+                                    <Field type="text" name="fullname" placeholder="ФИО" id="fullname" values={values.fullname} validate={validateFullName} />
 
+                                    <ErrorMessage name="email" component="div" className="form-error" />
                                     <Field type="email" name="email" placeholder="E-mail" id="email" values={values.email} />
-                                    <ErrorMessage name="email" component="div" />
 
+                                    <ErrorMessage name="phoneNumber" component="div" className="form-error" />
                                     <Field type="tel" name="phoneNumber" placeholder="Телефон" id="phoneNumber" values={values.phoneNumber} />
-                                    <ErrorMessage name="phoneNumber" component="div" />
 
+                                    <ErrorMessage name="address" component="div" className="form-error" />
                                     <div className="payment__wrapper-address">
                                         <Field type="text" name="address" placeholder="Населенный пункт, улица, дом" id="address" values={values.address} />
-                                        <ErrorMessage name="address" component="div" />
-
                                         <Field type="text" name="apartament" placeholder="Квартира" id="apartament" values={values.apartament} />
-                                        <ErrorMessage name="apartament" component="div" />
                                     </div>
 
+                                    <ErrorMessage name="zipcode" component="div" className="form-error" />
                                     <Field type="text" name="zipcode" placeholder="Индекс (6 цифр)" id="zipcode" values={values.zipcode} />
-                                    <ErrorMessage name="zipcode" component="div" />
 
+                                    <ErrorMessage name="contactUrl" component="div" className="form-error" />
                                     <Field type="text" name="contactUrl" placeholder="Ссылка на VK или Instagram для связи" id="contactUrl" values={values.contatUrl} />
-                                    <ErrorMessage name="contactUrl" component="div" />
 
                                     <button className="btn btn_wide" type="submit">Оплатить 3.840 ₽</button>
                                 </Form>
